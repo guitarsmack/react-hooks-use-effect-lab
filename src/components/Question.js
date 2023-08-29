5ea583fdb9701faa1afee4a51013a95b676bcfe8
Michael Alan Stowe <mastowe94@gmail.com>
2023-08-28 19:28:48 -0500

    complete lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() =>{
+     setTimeout(() => {
+      const timer = setInterval(() => {
+        setTimeRemaining((timeRemaining) => timeRemaining - 1)
+      }, 1000)
+
+      return function cleanup() {
+        clearInterval(timer)
+      }
+
+    },1000)
+    
+    
+
+  },[])
+
+  useEffect(() => {
+    if (timeRemaining < 0 ){
+      setTimeRemaining(10)
+      onAnswered(false)
+    }
+  }, [timeRemaining])
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
